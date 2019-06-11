eaad8adbe4498ecc4dcae025adca8d8d968f438f
PikminGuts92 <PikminGuts92@gmail.com>
2019-06-11 18:52:16 -0400

    Added csv to address conversion

--- src/lib.rs
+++ src/lib.rs
@@ -20,16 +20,73 @@ pub fn open_dir(data_dir: &str) {
         println!("{0}", f);
     }
 
+    let mut address_list = Vec::new();
+
     // Reads .csv files
     for f in &files {
-        let mut rdr = csv::Reader::from_path(Path::new(f));
+        let rdr = csv::Reader::from_path(Path::new(f));
         for result in rdr.unwrap().records() {
-            let record = result.unwrap();
-            println!("{:?}", record);
-            break;
+            let record: csv::StringRecord = result.unwrap();
+
+            let address = get_address_from_record(&record);
+            address_list.push(address);
         }
 
+        println!("Found {0} records!", address_list.len());
+
         break;
+    }
+}
+
+fn parse_float(s: &str) -> f64 {
+    match s.trim() {
+        "" => 0.0,
+        _ => match s.parse::<f64>() {
+            Ok(n) => n,
+            Err(_) => 0.0
+        },
+    }
+}
+
+fn get_address_from_record(record: &csv::StringRecord) -> Address {
+    Address {
+        longitude: match record.get(0) {
+            Some(value) => parse_float(value),
+            None => 0.0
+        },
+        latitude: match record.get(1) {
+            Some(value) => parse_float(value),
+            None => 0.0
+        },
+        number: match record.get(2) {
+            Some(value) => value.to_owned(),
+            None => String::new()
+        },
+        street: match record.get(3) {
+            Some(value) => value.to_owned(),
+            None => String::new()
+        },
+        unit: match record.get(4) {
+            Some(value) => value.to_owned(),
+            None => String::new()
+        },
+        city: match record.get(4) {
+            Some(value) => value.to_owned(),
+            None => String::new()
+        },
+        district: match record.get(5) {
+            Some(value) => value.to_owned(),
+            None => String::new()
+        },
+        region: match record.get(6) {
+            Some(value) => value.to_owned(),
+            None => String::new()
+        },
+        post_code: match record.get(7) {
+            Some(value) => value.to_owned(),
+            None => String::new()
+        },
+        hash: 0,
     }
 }
 
--- tests/csv_tests.rs
+++ tests/csv_tests.rs
@@ -6,19 +6,6 @@ use faradenza;
 mod tests {
     #[test]
     fn it_works_1() {
-      let add = faradenza::Address {
-        longitude: 0.0,
-        latitude: 0.0,
-        number: String::from(""),
-        street: String::from(""),
-        unit: String::from(""),
-        city: String::from(""),
-        district: String::from(""),
-        region: String::from(""),
-        post_code: String::from(""),
-        hash: 0,
-      };
-
       assert_eq!(2 + 2, 4);
     }
 
